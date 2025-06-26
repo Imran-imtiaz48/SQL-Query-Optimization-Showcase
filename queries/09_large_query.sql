caf0ee9a3e920b37d51556097eee0d68233498b5
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 07:03:38 +0400

    Update 09_large_query.sql

--- queries/09_large_query.sql
+++ queries/09_large_query.sql
@@ -1,2 +1,7 @@
--- 09 Large Query Example
-SELECT * FROM example_table;
+-- Aggregation of key KPIs
+SELECT 
+  (SELECT COUNT(*) FROM customers) AS total_customers,
+  (SELECT COUNT(*) FROM orders WHERE order_date >= CURDATE() - INTERVAL 30 DAY) AS orders_last_30_days,
+  (SELECT AVG(total_amount) FROM payment) AS avg_payment,
+  (SELECT SUM(stock) FROM inventory) AS total_stock
+FROM dual;
