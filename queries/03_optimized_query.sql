d66a34ccc4f87f4e9cd8cdce54a97931ba4f9840
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 06:53:02 +0400

    Update 03_optimized_query.sql
    QueryAdded

--- queries/03_optimized_query.sql
+++ queries/03_optimized_query.sql
@@ -1,2 +1,7 @@
--- 03 Optimized Query Example
-SELECT * FROM example_table;
+-- Optimized with DATE_FORMAT, indexed order_date
+SELECT DATE_FORMAT(o.order_date, '%Y-%m') AS order_month, p.name, SUM(oi.quantity) AS total_sold
+FROM orders o
+JOIN order_items oi ON o.id = oi.order_id
+JOIN products p ON oi.product_id = p.id
+GROUP BY DATE_FORMAT(o.order_date, '%Y-%m'), p.name
+ORDER BY total_sold DESC;
