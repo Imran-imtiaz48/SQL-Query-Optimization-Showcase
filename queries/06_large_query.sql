9b6da4acef5e8cc096c08aa4cecb4a1ee5aa0167
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 07:01:54 +0400

    Update 06_large_query.sql
    QueryAdded

--- queries/06_large_query.sql
+++ queries/06_large_query.sql
@@ -1,2 +1,5 @@
--- 06 Large Query Example
-SELECT * FROM example_table;
+-- Donation summary by donor type and time
+SELECT d.donor_type, YEAR(p.created_date) AS donation_year, SUM(p.total_amount) AS total_donated
+FROM payment p
+JOIN donors d ON p.donor_id = d.id
+GROUP BY d.donor_type, YEAR(p.created_date);
