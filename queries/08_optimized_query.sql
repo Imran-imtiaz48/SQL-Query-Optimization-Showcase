d14fd071630695df9f6d0ea573003daa02d2c1ee
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 07:03:20 +0400

    Update 08_optimized_query.sql

--- queries/08_optimized_query.sql
+++ queries/08_optimized_query.sql
@@ -1,2 +1,7 @@
--- 08 Optimized Query Example
-SELECT * FROM example_table;
+-- Optimized using conditional aggregation and LEFT JOIN
+SELECT u.id, u.username,
+       COUNT(CASE WHEN l.login_date = CURDATE() THEN 1 END) AS today_logins,
+       COUNT(CASE WHEN l.login_date >= CURDATE() - INTERVAL 7 DAY THEN 1 END) AS week_logins
+FROM users u
+LEFT JOIN logins l ON u.id = l.user_id
+GROUP BY u.id, u.username;
