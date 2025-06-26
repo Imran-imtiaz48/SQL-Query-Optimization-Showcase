d028379a62817684311349004657e3d25df623f2
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 06:51:30 +0400

    Update 02_large_query.sql
    QueryAdded

--- queries/02_large_query.sql
+++ queries/02_large_query.sql
@@ -1,2 +1,8 @@
--- 02 Large Query Example
-SELECT * FROM example_table;
+-- Payroll calculation with multiple joins and subqueries
+SELECT e.id, e.name, d.name AS department, 
+       (SELECT SUM(amount) FROM bonuses WHERE employee_id = e.id) AS bonus_total,
+       s.basic_salary + s.allowance AS total_salary
+FROM employees e
+JOIN departments d ON e.department_id = d.id
+JOIN salaries s ON e.id = s.employee_id
+WHERE s.month = '2024-12';
