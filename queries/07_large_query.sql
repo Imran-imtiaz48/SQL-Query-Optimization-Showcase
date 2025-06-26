ea0552ac55f6b3ff748b2162070c25c3020e1594
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 07:02:31 +0400

    Update 07_large_query.sql

--- queries/07_large_query.sql
+++ queries/07_large_query.sql
@@ -1,2 +1,6 @@
--- 07 Large Query Example
-SELECT * FROM example_table;
+-- Overdue invoice report with calculated days
+SELECT i.id, i.invoice_date, i.due_date, DATEDIFF(CURDATE(), i.due_date) AS days_overdue, c.name AS customer
+FROM invoices i
+JOIN customers c ON i.customer_id = c.id
+WHERE i.status = 'unpaid' AND i.due_date < CURDATE()
+ORDER BY days_overdue DESC;
