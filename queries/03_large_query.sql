d877363487cf67a76ee9bfe4585fafabe898daee
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 06:52:31 +0400

    Update 03_large_query.sql
    QueryAdded

--- queries/03_large_query.sql
+++ queries/03_large_query.sql
@@ -1,2 +1,7 @@
--- 03 Large Query Example
-SELECT * FROM example_table;
+-- Top selling products per month
+SELECT MONTH(o.order_date) AS order_month, p.name, SUM(oi.quantity) AS total_sold
+FROM orders o
+JOIN order_items oi ON o.id = oi.order_id
+JOIN products p ON oi.product_id = p.id
+GROUP BY MONTH(o.order_date), p.name
+ORDER BY total_sold DESC;
