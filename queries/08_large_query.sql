19106f042b568b2cceca6e62768b61ac40bf8b8f
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 07:03:04 +0400

    Update 08_large_query.sql

--- queries/08_large_query.sql
+++ queries/08_large_query.sql
@@ -1,2 +1,5 @@
--- 08 Large Query Example
-SELECT * FROM example_table;
+-- Daily and weekly user login counts
+SELECT u.id, u.username,
+  (SELECT COUNT(*) FROM logins l WHERE l.user_id = u.id AND l.login_date = CURDATE()) AS today_logins,
+  (SELECT COUNT(*) FROM logins l WHERE l.user_id = u.id AND l.login_date >= CURDATE() - INTERVAL 7 DAY) AS week_logins
+FROM users u;
