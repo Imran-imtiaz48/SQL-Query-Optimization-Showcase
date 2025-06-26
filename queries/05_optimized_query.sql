6daee322fb9388187ca5d21d6741d6e09e0e275e
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 06:54:46 +0400

    Update 05_optimized_query.sql
    QueryAdded

--- queries/05_optimized_query.sql
+++ queries/05_optimized_query.sql
@@ -1,2 +1,6 @@
--- 05 Optimized Query Example
-SELECT * FROM example_table;
+-- Optimized: assumes indexes on inventory.product_id and products.reorder_level
+SELECT p.id, p.name, i.stock, p.reorder_level
+FROM products p
+JOIN inventory i ON p.id = i.product_id
+WHERE i.stock < p.reorder_level
+ORDER BY i.stock ASC;
