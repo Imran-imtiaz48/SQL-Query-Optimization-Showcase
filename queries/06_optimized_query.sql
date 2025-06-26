70583a4dc78edcadc35e495615d50f7a5f8550dc
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-26 07:02:11 +0400

    Update 06_optimized_query.sql

--- queries/06_optimized_query.sql
+++ queries/06_optimized_query.sql
@@ -1,2 +1,5 @@
--- 06 Optimized Query Example
-SELECT * FROM example_table;
+-- Optimized: replace YEAR() with DATE_FORMAT if needed, and indexed date fields
+SELECT d.donor_type, DATE_FORMAT(p.created_date, '%Y') AS donation_year, SUM(p.total_amount) AS total_donated
+FROM payment p
+JOIN donors d ON p.donor_id = d.id
+GROUP BY d.donor_type, DATE_FORMAT(p.created_date, '%Y');
